b860c54f1005b604b23ceb50c2400e0ce36f1880
Leeflying <lixu_19900305@yeah.net>
2014-04-09 20:42:01 +0800

    Web客户端通讯录模块控制器

--- src/main/java/cn/edu/zju/isst/web/controller/RestaurantMenuController.java
+++ src/main/java/cn/edu/zju/isst/web/controller/RestaurantMenuController.java
@@ -14,11 +14,10 @@ import cn.edu.zju.isst.service.RestaurantMenuService;
 public class RestaurantMenuController {
     @Autowired
     private RestaurantMenuService restaurantMenuService;
-    
+
     @RequestMapping("/restaurants/{restaurantId}/menus.html")
-    public String list(Model model,
-            @PathVariable("restaurantId") int restaurantId) {
-        model.addAttribute("restaurants",restaurantMenuService.findAll(restaurantId)); 
+    public String list(Model model, @PathVariable("restaurantId") int restaurantId) {
+        model.addAttribute("restaurants", restaurantMenuService.findAll(restaurantId, 0, 1).getItems());
         return "restaurants/{restaurantId}/menus";
     }
 }
